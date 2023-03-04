18340f3765dc4a47520fa3d7094c625332c732d3
AgustinLaValla <agulavalla@gmail.com>
2023-03-04 19:19:31 -0300

    Add contact form UI

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,8 +1,9 @@
+import ContactForm from '@/components/contact/ContactForm'
 import React from 'react'
 
 export default function ContactPage() {
   return (
-    <div>contact</div>
+    <ContactForm/>
   )
 }
  
